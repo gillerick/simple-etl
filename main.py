cabf3cfb88e348a0b793d0ce26d1b92d5fbc8e9c
Gill Erick <ogayogill95@gmail.com>
2022-10-04 21:11:23 +0300

    Setup Spark session

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+import pyspark
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+spark = pyspark.sql.SparkSession \
+    .builder \
+    .appName("Python Spark SQL simple example") \
+    .config('spark.driver.extraClassPath', "postgresql-42.5.0.jar") \
+    .getOrCreate()

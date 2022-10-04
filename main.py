1d9abe335cb02562c863203b05b64e8328f2c866
Gill Erick <ogayogill95@gmail.com>
2022-10-04 21:15:48 +0300

    Read table from database using Spark jdbc

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ spark = pyspark.sql.SparkSession \
     .appName("Python Spark SQL simple example") \
     .config('spark.driver.extraClassPath', "postgresql-42.5.0.jar") \
     .getOrCreate()
+
+movies_df = spark.read \
+    .format("jdbc") \
+    .option("url", "jdbc:postgresql://localhost:5432/movies_db") \
+    .option("db_table", "movies") \
+    .option("user", "user") \
+    .option("password", "pass") \
+    .option("driver", "org.postgresql.Driver") \
+    .load()
+
+print(movies_df.show())
